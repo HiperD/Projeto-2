71a0fe0c76b021fec13bb47d85a61416265eba0a
Leonardo Silva e Cruz <101243664+HiperD@users.noreply.github.com>
2025-07-04 21:35:36 -0300

    final

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const btnJogoNumero = document.getElementById('jogoNumero');
-
 function iniciarJogoAdivinha(){
     alert('Seja bem-vindo ao nosso jogo!');
     let numeroSecreto = 5;
@@ -29,26 +27,32 @@ function iniciarJogoSoma(){
     alert(`A soma dos números é: ${numero}`);
 }
 
-function iniciarJogoMedia(){
+function iniciarJogoMedia(){ 
     alert("Bem-vindo ao Jogo da Média!");
+
     let quantidade = prompt("Quantos números você quer digitar?");
     quantidade = Number(quantidade);
+
     if (quantidade <= 0 || quantidade === null || quantidade === "" || typeof quantidade !== "number") {
-        alert("Quantidade inválida. Tente novamente com um número maior que zero.");
-    }
-    else {
-        let soma = 0;
-        for (let i = 1; i <= quantidade; i++) {
-            let entrada = prompt(`Digite o ${i}º número:`);
-            let numero = Number(entrada);
-            if (entrada.trim() === "" || entrada === null || isNaN(numero)) {
-            alert("Valor inválido. Digite um número.");
-            i--;
-            continue;
-            }
-            soma += numero;
+    alert("Quantidade inválida. Tente novamente com um número maior que zero.");
+    } else {
+    let soma = 0;
+
+    for (let i = 1; i <= quantidade; i++) {
+        let entrada = prompt(`Digite o ${i}º número:`);
+        let numero = Number(entrada);
+
+        if (entrada.trim() === "" || entrada === null || isNaN(numero)) {
+        alert("Valor inválido. Digite um número.");
+        i--; 
+        continue;
         }
+
+        soma += numero;
     }
+
     let media = soma / quantidade;
-    alert(`A média calculada entre os ${quantidade} números digitados é: ${media}`);
+
+    alert(`A média calculada entre os ${quantidade} números digitados é: ${media.toFixed(2)}`);
+    } 
 }
